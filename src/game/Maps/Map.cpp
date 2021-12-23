c229bdd60c65afa4187c36c7b2da54ac0102a646
celguar <celguar@gmail.com>
2021-12-23 23:45:49 +0300

    Crashfix attempt

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -1442,7 +1442,11 @@ void Map::RemoveAllObjectsInRemoveList()
     while (!i_objectsToRemove.empty())
     {
         WorldObject* obj = *i_objectsToRemove.begin();
+
         i_objectsToRemove.erase(i_objectsToRemove.begin());
+
+        if (!obj)
+            continue;
 
         switch (obj->GetTypeId())
         {
@@ -2337,10 +2341,12 @@ void Map::SendObjectUpdates()
     while (!i_objectsToClientUpdate.empty())
     {
         Object* obj = *i_objectsToClientUpdate.begin();
+
+        i_objectsToClientUpdate.erase(i_objectsToClientUpdate.begin());
+
         if (!obj)
             continue;
 
-        i_objectsToClientUpdate.erase(i_objectsToClientUpdate.begin());
         obj->BuildUpdateData(update_players);
     }
 
