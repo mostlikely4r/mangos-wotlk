12dbe67046b244455a275e098daf41990ea02a6e
celguar <celguar@gmail.com>
2021-12-23 23:51:04 +0300

    Save some info to armory feed
    Includes achievements, bosses killed, items

--- src/game/Achievements/AchievementMgr.cpp
+++ src/game/Achievements/AchievementMgr.cpp
@@ -2241,6 +2241,7 @@ void AchievementMgr::CompletedAchievement(AchievementEntry const* achievement)
         return;
 
     SendAchievementEarned(achievement);
+    GetPlayer()->CreateWowarmoryFeed(1, achievement->ID, 0, 0);
     CompletedAchievementData& ca =  m_completedAchievements[achievement->ID];
     ca.date = time(nullptr);
     ca.changed = true;
