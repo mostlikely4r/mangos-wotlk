f96b1aa3b3c31bf7a21090c5f489949f63c6643a
mostlikely4r <sebastiaan.keek@gmail.com>
2022-10-10 16:03:16 +0200

    -Performance: Lower the amount of packets/work being done for bot players.

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -493,8 +493,15 @@ bool Map::Add(Player* player)
     SendInitTransports(player);
 
     NGridType* grid = getNGrid(cell.GridX(), cell.GridY());
-    player->GetViewPoint().Event_AddedToWorld(&(*grid)(cell.CellX(), cell.CellY()));
-    UpdateObjectVisibility(player, cell, p);
+#ifdef ENABLE_PLAYERBOTS
+    if (player->isRealPlayer())
+    {
+#endif
+        player->GetViewPoint().Event_AddedToWorld(&(*grid)(cell.CellX(), cell.CellY()));
+        UpdateObjectVisibility(player, cell, p);
+#ifdef ENABLE_PLAYERBOTS
+    }
+#endif
 
 //Start Solocraft Functions
     if (sWorld.getConfig(CONFIG_BOOL_SOLOCRAFT_ENABLED))
