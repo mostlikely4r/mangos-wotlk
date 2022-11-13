2f095c2811ebe96b37c66718e0521a6ee3fdb608
mostlikely4r <sebastiaan.keek@gmail.com>
2022-11-13 22:21:56 +0100

    -Revert not loading social for bots until further notice

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -16829,18 +16829,6 @@ bool Player::LoadFromDB(ObjectGuid guid, SqlQueryHolder* holder)
     _LoadActions(holder->GetResult(PLAYER_LOGIN_QUERY_LOADACTIONS));
 
     m_social = sSocialMgr.LoadFromDB(holder->GetResult(PLAYER_LOGIN_QUERY_LOADSOCIALLIST), GetObjectGuid());
-#ifdef ENABLE_PLAYERBOTS
-    if (!isRealPlayer())
-        m_social = sSocialMgr.LoadFromDB(holder->GetResult(PLAYER_LOGIN_QUERY_LOADSOCIALLIST), GetObjectGuid());
-    else
-    {
-#endif
-
-        m_social = new PlayerSocial();
-        m_social->SetPlayerGuid(GetObjectGuid());
-#ifdef ENABLE_PLAYERBOTS
-    }
-#endif
 
     // check PLAYER_CHOSEN_TITLE compatibility with PLAYER__FIELD_KNOWN_TITLES
     // note: PLAYER__FIELD_KNOWN_TITLES updated at quest status loaded
